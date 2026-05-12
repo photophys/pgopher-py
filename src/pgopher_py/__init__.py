e3bd0c780cbec9126de25110c6b3c057cd7b9042
Felix Moeller <felix.moeller@student.uibk.ac.at>
2026-05-12 13:52:14 +0200

    uncommented init

--- src/pgopher_py/__init__.py
+++ src/pgopher_py/__init__.py
@@ -4,15 +4,15 @@
 Python interface for PGOPHER.
 """
 
-# from .core import simulate_spectrum
-# from .datatypes import LinearGroundState, LinearExcitedState, Lambda, Parity
-# from .parse import PgopherSpectrum
+from .core import simulate_spectrum
+from .datatypes import LinearGroundState, LinearExcitedState, Lambda, Parity
+from .parse import PgopherSpectrum
 
-# __all__ = [
-#     "simulate_spectrum",
-#     "LinearGroundState",
-#     "LinearExcitedState",
-#     "Lambda",
-#     "Parity",
-#     "PgopherSpectrum",
-# ]
+__all__ = [
+    "simulate_spectrum",
+    "LinearGroundState",
+    "LinearExcitedState",
+    "Lambda",
+    "Parity",
+    "PgopherSpectrum",
+]
